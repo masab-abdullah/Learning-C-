3c295eb7ceb2bff3340c5c3b72a66001288f4221
masab-abdullah <m.masababdullah@gmail.com>
2026-03-30 11:08:39 +0500

    added 5 c++ files

---
+++ adding.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mynum = 7;
+    int urnum = 4;
+    
+    int sum = mynum + urnum;
+
+    cout << "the sum of" << mynum <<  " and "  << urnum <<  " is "  << sum << endl;
+    return 0;
+}
---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int age = 18;
+
+    cout<< "hello abdullah" <<endl;
+    cout<< "your age is" <<age <<endl;
+    return 0;
+}
