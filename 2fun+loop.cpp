cd747b1bb2a2f13972291ffea4a77514ce27651d
masab-abdullah <m.masababdullah@gmail.com>
2026-04-16 13:10:52 +0500

    new

---
+++ 2fun+loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int add(int x)
+{
+    return x + 2;
+}
+
+int main()
+{
+    int a = 3;
+
+    for(int i = 1; i <= 3; i++)
+    {
+        a = add(a);
+        cout << a << " ";
+    }
+}
