bbbd60951310fdfb4d1748a266417a4407cda3f1
masab-abdullah <m.masababdullah@gmail.com>
2026-04-17 11:02:07 +0500

    done

---
+++ switxh.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fun( int x )
+{
+    switch(x % 5)
+    {
+    case 0: return x / 2;
+    case 1: return x * 2;
+    }
+    return 0;
+}
+int main() {
+    for(int i =2; i <=5;i++)
+    {
+        cout << fun(i) << " ";
+    }
+}
