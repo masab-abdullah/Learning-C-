d0506ead5ec2cc8038789f7084f3bbacbf596aea
masab-abdullah <m.masababdullah@gmail.com>
2026-05-11 12:51:19 +0500

    done

---
+++ large.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[5];
+
+    for(int i = 0; i < 5;i++) {
+
+        cin >> arr[i];
+    }
+    int largest = arr[0];
+
+    for(int i = 0; i < 5; i++ ) {
+
+        if (arr[i] > largest){
+
+        largest = arr[i]; 
+       }
+    }
+    cout <<  "largest number is :" << largest;
+
+    return 0;
+
+}
